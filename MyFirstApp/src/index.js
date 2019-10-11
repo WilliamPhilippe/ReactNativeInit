f201aebd0dd1c88d222c452029c5372eaaf9e630
William Philippe <wplb@ic.ufal.br>
2019-10-11 20:27:55 -0300

    free

---
+++ MyFirstApp/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {View} from 'react-native';
+
+import './config/ReacttotronConfig';
+import Routes from './routes';
+
+export default function App() {
+  return <Routes />;
+}
