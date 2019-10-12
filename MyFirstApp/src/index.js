999e2c10fd8ddfda770fbc404649e2513cc390a4
William Philippe <wplb@ic.ufal.br>
2019-10-12 00:02:38 -0300

    mornig

--- MyFirstApp/src/index.js
+++ MyFirstApp/src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import {View} from 'react-native';
+import {StatusBar} from 'react-native';
 
 import './config/ReacttotronConfig';
 import Routes from './routes';
 
 export default function App() {
-  return <Routes />;
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <Routes />
+    </>
+  );
 }
--- MyFirstApp/src/pages/Main/index.js
+++ MyFirstApp/src/pages/Main/index.js
@@ -1,10 +1,57 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {Component} from 'react';
+import {Keyboard} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {Container, Form, Input, SubmitButton} from './styles';
+import Api from '../../services/api';
 
-// import { Container } from './styles';
+export default class Main extends Component {
+  state = {
+    newUser: '',
+    users: [],
+  };
 
-export default function Main() {
-  return <View />;
+  handleAddUser = async () => {
+    const {users, newUser} = this.state;
+
+    const response = await Api.get(`/users/${newUser}`);
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar,
+    };
+
+    this.setState({
+      users: [...users, data],
+      newUser: '',
+    });
+
+    Keyboard.dismiss();
+  };
+
+  render() {
+    const {users, newUser} = this.state;
+
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Insira o usuário"
+            value={newUser}
+            onChangeText={text => this.setState({newUser: text})}
+            returnKeyType="send"
+            onSubmitEditing={this.handleAddUser}
+          />
+          <SubmitButton onPress={this.handleAddUser}>
+            <Icon name="add" color="#eee" />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
 
 Main.navigationOptions = {
