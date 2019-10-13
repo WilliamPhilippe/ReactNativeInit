61c6c0d0061d51d72e187dd3c1a076889a97f9c9
William Philippe <wplb@ic.ufal.br>
2019-10-12 21:32:35 -0300

    finished

--- MyFirstApp/src/pages/Main/index.js
+++ MyFirstApp/src/pages/Main/index.js
@@ -1,17 +1,52 @@
+/* eslint-disable react/sort-comp */
+/* eslint-disable react/no-typos */
 import React, {Component} from 'react';
-import {Keyboard} from 'react-native';
+import {Keyboard, ActivityIndicator} from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import PropTypes from 'prop-types';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import {Container, Form, Input, SubmitButton} from './styles';
+import {
+  Container,
+  Form,
+  Input,
+  SubmitButton,
+  List,
+  Avatar,
+  User,
+  ProfileButton,
+  ProfileButtonText,
+  Name,
+  Bio,
+} from './styles';
 import Api from '../../services/api';
 
 export default class Main extends Component {
   state = {
     newUser: '',
     users: [],
+    loading: false,
   };
+
+  async componentDidMount() {
+    const users = await AsyncStorage.getItem('users');
+
+    if (users) {
+      this.setState({users: JSON.parse(users)});
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const {users} = this.state;
+
+    if (prevState.users !== users) {
+      AsyncStorage.setItem('users', JSON.stringify(users));
+    }
+  }
 
   handleAddUser = async () => {
     const {users, newUser} = this.state;
+
+    this.setState({loading: true});
 
     const response = await Api.get(`/users/${newUser}`);
 
@@ -19,7 +54,7 @@ export default class Main extends Component {
       name: response.data.name,
       login: response.data.login,
       bio: response.data.bio,
-      avatar: response.data.avatar,
+      avatar: response.data.avatar_url,
     };
 
     this.setState({
@@ -27,11 +62,29 @@ export default class Main extends Component {
       newUser: '',
     });
 
+    this.setState({loading: false});
+
     Keyboard.dismiss();
   };
 
+  handleNavigate = user => {
+    const {navigation} = this.props;
+
+    navigation.navigate('User', {user});
+  };
+
+  static navigationOptions = {
+    title: 'Usuários',
+  };
+
+  static propTypes = {
+    navigation: PropTypes.shape({
+      navigate: PropTypes.func,
+    }).isRequired,
+  };
+
   render() {
-    const {users, newUser} = this.state;
+    const {users, newUser, loading} = this.state;
 
     return (
       <Container>
@@ -45,15 +98,31 @@ export default class Main extends Component {
             returnKeyType="send"
             onSubmitEditing={this.handleAddUser}
           />
-          <SubmitButton onPress={this.handleAddUser}>
-            <Icon name="add" color="#eee" />
+          <SubmitButton loading={loading} onPress={this.handleAddUser}>
+            {loading ? (
+              <ActivityIndicator color="#fff" />
+            ) : (
+              <Icon name="add" color="#eee" />
+            )}
           </SubmitButton>
         </Form>
+
+        <List
+          data={users}
+          keyExtractor={user => user.login}
+          renderItem={({item}) => (
+            <User>
+              <Avatar source={{uri: item.avatar}} />
+              <Name>{item.name}</Name>
+              <Bio>{item.bio}</Bio>
+
+              <ProfileButton onPress={() => this.handleNavigate(item)}>
+                <ProfileButtonText>Ver perfil</ProfileButtonText>
+              </ProfileButton>
+            </User>
+          )}
+        />
       </Container>
     );
   }
 }
-
-Main.navigationOptions = {
-  title: 'Usuários',
-};
--- MyFirstApp/src/pages/User/index.js
+++ MyFirstApp/src/pages/User/index.js
@@ -1,12 +1,71 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import Api from '../../services/api';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+} from './styles';
 
-export default function Main() {
-  return <View />;
+export default class Main extends Component {
+  static navigationOptions = ({navigation}) => ({
+    title: navigation.getParam('user').name,
+  });
+
+  static propTypes = {
+    navigation: PropTypes.shape({
+      getParam: PropTypes.func,
+    }).isRequired,
+  };
+
+  state = {
+    stars: [],
+  };
+
+  async componentDidMount() {
+    const {navigation} = this.props;
+    const user = navigation.getParam('user');
+
+    const response = await Api.get(`/users/${user.login}/starred`);
+
+    this.setState({stars: response.data});
+  }
+
+  render() {
+    const {stars} = this.state;
+    const user = this.props.navigation.getParam('user');
+
+    return (
+      <Container>
+        <Header>
+          <Avatar source={{uri: user.avatar}} />
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+
+        <Stars
+          data={stars}
+          keyExtractor={star => String(star.id)}
+          renderItem={({item}) => (
+            <Starred>
+              <OwnerAvatar source={{uri: item.owner.avatar_url}} />
+              <Info>
+                <Title>{item.name}</Title>
+                <Author>{item.owner.login}</Author>
+              </Info>
+            </Starred>
+          )}
+        />
+      </Container>
+    );
+  }
 }
-
-Main.navigationOptions = {
-  titile: 'User',
-};
